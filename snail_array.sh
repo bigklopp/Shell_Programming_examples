19fca841abbf1288fe923a483affb3d579028c93
LHC <zmxncbv14@gmail.com>
2021-07-02 09:00:53 +0900

    210702 !!

--- snail_array.sh
+++ snail_array.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-
-#array1=(11 12 13 14 15)
-#array2=(21 22 23 24 25)
-#array3=(31 32 33 34 35)
-#array4=(41 42 43 44 45)
-#array5=(51 52 53 54 55)
-#array=(array1 array2 array3 array4 array5)
 k=0
 n=5
-array=()
-for ((i=1;i<=5;i++))
+m=5
+i=1
+while [ ${k} != 25 ]
 do
-#array+=(${i})
-#echo ${array[*]}
-	for ((j=1;j<=5;j++))
+	for ((j=1;j<=${n};j++))
 	do
 		let "k+=1"
+		
+
+		Line=${array[$i]}
+		Line[$j]=${k}
+		array+=("${k}")
 		if [ ${j} -eq ${n} ]
 		then
 			let "n-=1"
 			break
 		fi
-		array+=("${k}")
-		echo $k
-		echo ${array[*]}
 	done
+	for ((i=1;i<=${n};i++))
+	do
+		let "k+=1"
+		array2+=("${k}")
+	done
+	break
 done
+echo ${array[*]}
+echo ${array2[*]}
+echo ${array3[*]}
+echo ${array4[*]}
+#echo ${array[*]}
